260ebd45e4a022a594111fab2b32e3b6e7f34f5b
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-02 12:16:09 +0300

    first commit

---
+++ src/main/java/java12/final/cryptowin/CryptowinApplication.java
@@ -0,0 +1,13 @@
+package java12.final.cryptowin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptowinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptowinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java12/final/cryptowin/CryptowinApplicationTests.java
@@ -0,0 +1,16 @@
+package java12.final.cryptowin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CryptowinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
