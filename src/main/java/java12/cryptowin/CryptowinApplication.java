8b540f22a68d9a6847f8c599619bd72ea9d0bda0
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-02 16:59:34 +0300

    renamed package final to java12.cryptowin

--- src/main/java/java12/cryptowin/CryptowinApplication.java
+++ src/main/java/java12/cryptowin/CryptowinApplication.java
@@ -1,7 +1,8 @@
-package java12.final.cryptowin;
+package java12.cryptowin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CryptowinApplication {
