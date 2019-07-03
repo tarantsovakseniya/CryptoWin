f49ae14e89e2e9710f56f99ca406fd4414fb10b3
ekate <ekatereanagricaenko@gmail.com>
2019-07-03 19:27:30 +0300

    add class (CryptoMonitorService) with basic methods to package service

---
+++ src/main/java/java12/cryptowin/repository/CryptoMonitorRepository.java
@@ -0,0 +1,7 @@
+package java12.cryptowin.repository;
+
+import java12.cryptowin.entity.CryptoMonitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CryptoMonitorRepository extends JpaRepository<CryptoMonitor,Long> {
+}
