a5c8bef7dc41120df80b67e589a1dd538abb0d76
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-04 11:56:43 +0300

    changes into class exchange

--- src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
+++ src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
@@ -1,15 +1,82 @@
 package java12.cryptowin.entity.enumClasses;
 
 public enum CryptoExchange {
-    Binance (""), Poloniex (""), EXMO ("");
+    POLONIEX(""),
+    BINANCE(""),
+    EXMO(""),
+    CRYPTO_BRIDGE(""),
+    GRAVIEX("");
 
-   private String url;
+    private String name;
+    private String url;
+
+    private double minDepositSum;
+    private double maxDepositSum;
+
+    private String minWithdrawSum;
+    private double maxWithdrawSum;
+
+    private String withdrawCommission;
+    private double depositCommission;
+
+    private String transactionCommission;
+
 
     CryptoExchange(String url) {
         this.url = url;
+
+    }
+
+//    CryptoExchange(String name, String url,
+//                   double minDepositSum, double maxDepositSum,
+//                   String minWithdrawSum, double maxWithdrawSum,
+//                   String withdrawCommission, double depositCommission,
+//                   String transactionCommission) {
+//        this.name = name;
+//        this.url = url;
+//        this.minDepositSum = minDepositSum;
+//        this.maxDepositSum = maxDepositSum;
+//        this.minWithdrawSum = minWithdrawSum;
+//        this.maxWithdrawSum = maxWithdrawSum;
+//        this.withdrawCommission = withdrawCommission;
+//        this.depositCommission = depositCommission;
+//        this.transactionCommission = transactionCommission;
+//    }
+
+    public String getName() {
+        return name;
     }
 
     public String getUrl() {
         return url;
+    }
+
+
+    public double getMinDepositSum() {
+        return minDepositSum;
+    }
+
+    public double getMaxDepositSum() {
+        return maxDepositSum;
+    }
+
+    public String getMinWithdrawSum() {
+        return minWithdrawSum;
+    }
+
+    public double getMaxWithdrawSum() {
+        return maxWithdrawSum;
+    }
+
+    public String getWithdrawCommission() {
+        return withdrawCommission;
+    }
+
+    public double getDepositCommission() {
+        return depositCommission;
+    }
+
+    public String getTransactionCommission() {
+        return transactionCommission;
     }
 }
