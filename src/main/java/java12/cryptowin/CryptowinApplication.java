80d0971e429adc04cf55f3affb62bcf31d3fb532
Sviatoslav78 <gaevoy70@gmail.com>
2019-07-04 11:38:33 +0300

    changed name of the class to cryptowinApplication

--- src/main/java/java12/cryptowin/CryptowinApplication.java
+++ src/main/java/java12/cryptowin/CryptowinApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class CryptoWinApplication {
+public class CryptowinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptoWinApplication.class, args);
+		SpringApplication.run(CryptowinApplication.class, args);
 	}
 
 }
