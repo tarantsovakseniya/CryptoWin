68bb2c7ed8667f7e1ae4198980c39cb8b3e62688
Kseniya Tarantsova <43914783+tarantsovakseniya@users.noreply.github.com>
2019-07-04 12:06:45 +0300

    Revert "Model and view"

--- src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
+++ src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
@@ -1,15 +1,4 @@
 package java12.cryptowin.entity.enumClasses;
 
 public enum CryptoExchange {
-    Binance (""), Poloniex (""), EXMO ("");
-
-   private String url;
-
-    CryptoExchange(String url) {
-        this.url = url;
-    }
-
-    public String getUrl() {
-        return url;
-    }
 }
