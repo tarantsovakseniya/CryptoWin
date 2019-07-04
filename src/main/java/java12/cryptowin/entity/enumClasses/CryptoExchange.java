737e13545748164b1cde0b93e7f93a3b626cbce4
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-04 10:44:50 +0300

    last changes in main and merge with Katherina branch

--- src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
+++ src/main/java/java12/cryptowin/entity/enumClasses/CryptoExchange.java
@@ -1,4 +1,15 @@
 package java12.cryptowin.entity.enumClasses;
 
 public enum CryptoExchange {
+    Binance (""), Poloniex (""), EXMO ("");
+
+   private String url;
+
+    CryptoExchange(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
