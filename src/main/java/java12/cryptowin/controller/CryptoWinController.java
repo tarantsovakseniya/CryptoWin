1f07a82fb0d868aa011ff1d4d8d9ecbbae0e0805
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-07 17:01:23 +0300

    start to do subcribe func

--- src/main/java/java12/cryptowin/controller/CryptoWinController.java
+++ src/main/java/java12/cryptowin/controller/CryptoWinController.java
@@ -3,18 +3,14 @@ package java12.cryptowin.controller;
 import java12.cryptowin.entity.CryptoMonitor;
 import java12.cryptowin.entity.enumClasses.CryptCoinType;
 import java12.cryptowin.entity.enumClasses.CryptoExchange;
-import java12.cryptowin.entity.enumClasses.TimeType;
 import java12.cryptowin.service.jpa.CryptoMonitorService;
+import java12.cryptowin.service.jpa.BetterOfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
-import java12.cryptowin.service.jpa.BetterOfferService;
 import java.util.*;
 
 @Controller
@@ -25,63 +21,6 @@ public class CryptoWinController {
 
     @Autowired
     BetterOfferService betterOfferService;
-
-    @RequestMapping(value = "/charts", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView getCharts(@RequestParam( name = "coinType", required = false) String coinType,
-                                  @RequestParam(name = "exchangeType", required = false) String  exchangeType,
-                                  @RequestParam(name = "timeType", required = false) String timeType){
-        ModelAndView result = new  ModelAndView("chart");
-        if(coinType==null){
-            coinType = CryptCoinType.BITCOIN.getNameOfCoin();
-        }
-        if(exchangeType == null){
-            exchangeType = CryptoExchange.BINANCE.getName();
-        }
-        if(timeType == null){
-            timeType = TimeType.WEEK.getName();
-        }
-
-        List<CryptoMonitor> crypto = fillListToUserRequest(coinType, timeType, exchangeType, cryptoMonitorService.getAll());
-        //cryptoMonitorService.getByCryptoAndExName(coinType, exchangeType);
-        result.addObject("crypto", crypto);
-        result.addObject("coins", CryptCoinType.values());
-        result.addObject("exchanges", CryptoExchange.values());
-        result.addObject("times", TimeType.values());
-        result.addObject("timeType", timeType);
-        result.addObject("exchangeType", exchangeType);
-        result.addObject("coinType", coinType);
-
-        return result;
-
-    }
-
-    private List<CryptoMonitor> fillListToUserRequest(String coinType, String timeType, String exchangeType, List<CryptoMonitor> all) {
-        all.forEach(cryptoMonitor -> {
-            if(!cryptoMonitor.getCoinType().getNameOfCoin().equals(coinType)){
-                all.remove(cryptoMonitor);
-            }
-
-            if(!cryptoMonitor.getExchange().getName().equals(exchangeType)){
-                all.remove(cryptoMonitor);
-            }
-
-            LocalDate localDate = LocalDate.now();
-            if(timeType.equals(TimeType.YEAR.getName())){
-                localDate = localDate.minusYears(1);
-            }
-            if(timeType.equals(TimeType.MONTH.getName())){
-                localDate = localDate.minusMonths(1);
-            }
-            if(timeType.equals(TimeType.WEEK.getName())){
-                localDate.minusWeeks(1);
-            }
-
-            if(cryptoMonitor.getDate().isBefore(localDate)){
-                all.remove(cryptoMonitor);
-            }
-        });
-        return all;
-    }
 
     @GetMapping(value = {"/main", "/"})
     public ModelAndView getMain() {
@@ -112,7 +51,7 @@ public class CryptoWinController {
     }
 
     @GetMapping(value = {"/main/better-offer", "/better-offer"})
-    public ModelAndView postBetterOffer(@RequestParam("cryptCoin") CryptCoinType cryptCoin) {
+    public ModelAndView getBetterOffer(@RequestParam("cryptCoin") CryptCoinType cryptCoin) {
         ModelAndView result = new ModelAndView("better-offer");
 
 //        List<CryptoMonitor> items = cryptoMonitorService.getAll();
