11592a0c984939f75cb82f0543066f4e51ce70fe
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-07 15:27:11 +0300

    cryptowin

--- src/main/java/java12/cryptowin/controller/CryptoWinController.java
+++ src/main/java/java12/cryptowin/controller/CryptoWinController.java
@@ -8,11 +8,8 @@ import java12.cryptowin.service.jpa.CryptoMonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import sun.reflect.generics.tree.Tree;
 
 import java.util.*;
 
