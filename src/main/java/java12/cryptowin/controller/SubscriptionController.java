d33f8b5b2d7872dd9bb83fad0f676c59460f566e
ekate <ekatereanagricaenko@gmail.com>
2019-07-08 12:10:05 +0300

    add basic form settings to subscription.html and create regSubscription.html

---
+++ src/main/java/java12/cryptowin/controller/SubscriptionController.java
@@ -0,0 +1,37 @@
+package java12.cryptowin.controller;
+
+import java12.cryptowin.entity.User;
+import java12.cryptowin.entity.enumClasses.CryptCoinType;
+import java12.cryptowin.service.jpa.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping(value = "/subscription")
+public class SubscriptionController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    @GetMapping
+    public ModelAndView getSubscription(@RequestParam(value = "error", required = false) String error){
+        ModelAndView result = new ModelAndView("subscription");
+        result.addObject("coins", CryptCoinType.values());
+        result.addObject("error", error);
+        return result;
+    }
+
+    @GetMapping("/addNew")
+    public ModelAndView getFormForAdd(@RequestParam("coin") CryptCoinType coinType,
+                                      @RequestParam(value = "user") User user){
+        ModelAndView result = new ModelAndView("regSubscription");
+        return result;
+
+    }
+
+}
