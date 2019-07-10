78e58eaed5a0f4aaae8b54ad26412792b28b2865
ekate <ekatereanagricaenko@gmail.com>
2019-07-10 21:18:33 +0300

    finish subscription html

--- src/main/java/java12/cryptowin/controller/SubscriptionController.java
+++ src/main/java/java12/cryptowin/controller/SubscriptionController.java
@@ -1,11 +1,14 @@
 package java12.cryptowin.controller;
 
+import java12.cryptowin.entity.Subscription;
 import java12.cryptowin.entity.User;
 import java12.cryptowin.entity.enumClasses.CryptCoinType;
 import java12.cryptowin.service.jpa.SubscriptionService;
+import java12.cryptowin.service.jpa.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,18 +21,48 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService subscriptionService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ModelAndView getSubscription(@RequestParam(value = "error", required = false) String error){
+    public ModelAndView getSubscription(@RequestParam(value = "error", required = false) String error) {
         ModelAndView result = new ModelAndView("subscription/subscription");
         result.addObject("coins", CryptCoinType.values());
+        result.addObject("error", error);
+        result.addObject("user", userService.getCurrentUser());
+        return result;
+    }
+
+    @PostMapping()
+    public ModelAndView getAdd(@RequestParam("minResult") Long minResult,
+                               @RequestParam("maxResult") Long maxResult,
+                               @RequestParam(value = "coinType") CryptCoinType coinType) {
+        ModelAndView result = new ModelAndView("subscription/subscription");
+        result.addObject("user", userService.getCurrentUser());
+        result.addObject("coin", coinType);
+        result.addObject("coins", CryptCoinType.values());
+
+        String error = null;
+        User user = userService.getCurrentUser();
+        if (minResult == null || maxResult == null || coinType == null) {
+            error = "incorrect";
+        } else {
+                subscriptionService.save(new Subscription(user, coinType, minResult, maxResult));
+                error = "success";
+        }
         result.addObject("error", error);
         return result;
     }
 
+
     @GetMapping("/addNew")
     public ModelAndView getFormForAdd(@RequestParam(value = "coin") CryptCoinType coinType,
-                                      @RequestParam(value = "user", required = false) User user){
-        ModelAndView result = new ModelAndView("subscription/regSubscription");
+                                      @RequestParam("error") String error) {
+        ModelAndView result = new ModelAndView("subscription/subscription");
+        result.addObject("user", userService.getCurrentUser());
+        result.addObject("coin", coinType);
+        result.addObject("error", error);
+        result.addObject("coins", CryptCoinType.values());
         return result;
 
     }
