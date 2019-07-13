14fc8cbeefecae882273081a5291361390cc796a
ekate <ekatereanagricaenko@gmail.com>
2019-07-13 20:19:07 +0300

    add chart with ajax

--- src/main/java/java12/cryptowin/controller/ChartsController.java
+++ src/main/java/java12/cryptowin/controller/ChartsController.java
@@ -1,5 +1,6 @@
 package java12.cryptowin.controller;
 
+import com.google.gson.JsonArray;
 import java12.cryptowin.entity.CryptoMonitor;
 import java12.cryptowin.entity.enumClasses.CryptCoinType;
 import java12.cryptowin.entity.enumClasses.CryptoExchange;
@@ -10,12 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDateTime;
-
-import java.util.Comparator;
 
 import java.util.List;
 
@@ -32,16 +27,17 @@ public class ChartsController {
 
     @PostMapping
     public ModelAndView postData(@RequestParam(name = "coinType") String coinType,
-                                  @RequestParam(name = "exchangeType") String exchangeType,
-                                  @RequestParam(name = "timeType") String timeType,
-                                  @RequestParam(name = "state") String state) {
+                                 @RequestParam(name = "exchangeType") String exchangeType,
+                                 @RequestParam(name = "timeType") String timeType,
+                                 @RequestParam(name = "state") String state) {
         ModelAndView result = new ModelAndView("chart");
         String error = null;
-        if (coinType.equals("") || exchangeType.equals("") || timeType.equals("") || state.equals("")){
+        if (coinType.equals("") || exchangeType.equals("") || timeType.equals("") || state.equals("")) {
             error = "error";
         }
 
         List<CryptoMonitor> crypto = cryptoMonitorService.fillListToUserRequest(coinType, timeType, exchangeType);
+
         result.addObject("crypto", crypto);
         result.addObject("coins", CryptCoinType.values());
         result.addObject("exchanges", CryptoExchange.values());
@@ -50,19 +46,20 @@ public class ChartsController {
         result.addObject("exchange", exchangeType);
         result.addObject("coin", coinType);
         result.addObject("state", state);
-        result.addObject("error",error);
+        result.addObject("error", error);
         result.addObject("user", userService.getCurrentUser());
 
         return result;
 
     }
 
+
     @GetMapping
     public ModelAndView getCharts(@RequestParam(name = "coin", required = false) String coinType,
                                   @RequestParam(name = "exchange", required = false) String exchangeType,
                                   @RequestParam(name = "time", required = false) String timeType,
                                   @RequestParam(name = "state", required = false) String state,
-                                  @RequestParam(name = "error", required = false) String error){
+                                  @RequestParam(name = "error", required = false) String error) {
         ModelAndView result = new ModelAndView("chart");
         if (coinType == null) {
             coinType = CryptCoinType.BITCOIN.getNameOfCoin();
@@ -73,9 +70,10 @@ public class ChartsController {
         if (timeType == null) {
             timeType = TimeType.WEEK.getName();
         }
-        if(state == null){
+        if (state == null) {
             state = "sell";
         }
+
 
         List<CryptoMonitor> crypto = cryptoMonitorService.fillListToUserRequest(coinType, timeType, exchangeType);
         result.addObject("crypto", crypto);
@@ -86,7 +84,7 @@ public class ChartsController {
         result.addObject("exchange", exchangeType);
         result.addObject("coin", coinType);
         result.addObject("state", state);
-        result.addObject("user",userService.getCurrentUser());
+        result.addObject("user", userService.getCurrentUser());
 
         return result;
 
