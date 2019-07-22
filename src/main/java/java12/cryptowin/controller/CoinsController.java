71f006352c04eebd236137778f307d01ab56280c
Kseniya Tarantsova <kseniya.tarantsova@gmail.com>
2019-07-22 14:40:14 +0300

    coin-exchange done

--- src/main/java/java12/cryptowin/controller/CoinsController.java
+++ src/main/java/java12/cryptowin/controller/CoinsController.java
@@ -4,6 +4,7 @@ import java12.cryptowin.entity.CryptoMonitor;
 import java12.cryptowin.entity.enumeration.CryptCoinType;
 import java12.cryptowin.entity.enumeration.CryptoExchange;
 import java12.cryptowin.pojo.CryptoMonitorResult;
+import java12.cryptowin.service.jpa.CryptCoinExchangeService;
 import java12.cryptowin.service.jpa.CryptoMonitorService;
 import java12.cryptowin.service.jpa.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CoinsController {
@@ -25,6 +27,9 @@ public class CoinsController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CryptCoinExchangeService coinExchangeService;
 
     @GetMapping(value = "/stats")
     public ModelAndView getCoinInfo(@RequestParam("cryptCoin") CryptCoinType coinType,
@@ -38,41 +43,14 @@ public class CoinsController {
             }
         });
 
-        List<CryptoMonitor> cryptoToday = new ArrayList<>();
-        List<CryptoMonitor> cryptoAll = cryptoMonitorService.getAll();
-        cryptoAll.forEach(cryptoMonitor -> {
-            if (cryptoMonitor.getDate().toLocalDate().equals(LocalDate.now())) {
-                cryptoToday.add(cryptoMonitor);
-            }
-        });
-
-        final double[] maxPriceBuy = {0};
-        final double[] minPriceSell = {0};
-        final double[] minPriceBuy = {0};
-        final double[] maxPriceSell = {0};
-        cryptoToday.forEach(cryptoMonitor -> {
-            if (cryptoMonitor.getCoinType().equals(coinType) && cryptoMonitor.getExchange().equals(cryptoExchange)) {
-                if (cryptoMonitor.getSellingRate() < minPriceSell[0]) {
-                    minPriceSell[0] = cryptoMonitor.getSellingRate();
-                }
-                if (cryptoMonitor.getBuyingRate() > maxPriceBuy[0]) {
-                    maxPriceBuy[0] = cryptoMonitor.getBuyingRate();
-                }
-                if (cryptoMonitor.getSellingRate() > maxPriceSell[0]) {
-                    maxPriceSell[0] = cryptoMonitor.getSellingRate();
-                }
-                if (cryptoMonitor.getBuyingRate() < minPriceBuy[0]) {
-                    maxPriceSell[0] = cryptoMonitor.getBuyingRate();
-                }
-            }
-        });
+        Map<String,Double> minMaxResult= coinExchangeService.getMaxMinResultToday(cryptoMonitorService.getAll(),coinType,cryptoExchange);
 
         result.addObject("items", cryptoMonitorService);
         result.addObject("user", userService.getCurrentUser());
-        result.addObject("minSell",minPriceSell[0]);
-        result.addObject("maxBuy",maxPriceBuy[0]);
-        result.addObject("maxSell",maxPriceSell[0]);
-        result.addObject("minBuy",minPriceBuy[0]);
+        result.addObject("minSell",minMaxResult.get("minSell"));
+        result.addObject("maxBuy",minMaxResult.get("maxBuy"));
+        result.addObject("maxSell",minMaxResult.get("maxSell"));
+        result.addObject("minBuy",minMaxResult.get("minBuy"));
 
         return result;
     }
