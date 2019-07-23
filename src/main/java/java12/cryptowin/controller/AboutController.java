a395a73cb21c4e4e78e1f58cd325f1864d15da2d
natalie-cafune <nataly.pavelko@gmail.com>
2019-07-23 20:56:36 +0300

    fixed about page

--- src/main/java/java12/cryptowin/controller/AboutController.java
+++ src/main/java/java12/cryptowin/controller/AboutController.java
@@ -1,5 +1,7 @@
 package java12.cryptowin.controller;
 
+import java12.cryptowin.service.jpa.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AboutController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping(value = "/about")
     public ModelAndView getAboutPage() {
-        ModelAndView modelAndView = new ModelAndView("about");
-        return modelAndView;
+        ModelAndView result = new ModelAndView("about");
+        result.addObject("user", userService.getCurrentUser());
+        return result;
     }
 }
